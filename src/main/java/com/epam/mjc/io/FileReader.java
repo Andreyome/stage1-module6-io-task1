892fda6ade0c773f2c6c4c7af50bab96e3f4276f
Andreyome <andreyomem@gmail.com>
2023-01-07 19:12:31 +0200

    Commit 1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,27 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new java.io.FileReader("src/main/resources/Profile.txt"));
+            String [] line = (reader.readLine()).split(": ");
+            String name =line [1];
+            String [] line1 = (reader.readLine()).split(": ");
+            int age = Integer.parseInt(line1 [1]);
+            String [] line2 = (reader.readLine()).split(": ");
+            String  e_mail = line2 [1];
+            String [] line3 = (reader.readLine()).split(": ");
+            long phone = Long.parseLong(line3 [1]);
+            System.out.println(name +" "+ age+" "+e_mail+" "+phone);
+            return new Profile(name,age,e_mail,phone);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
         return new Profile();
     }
 }
