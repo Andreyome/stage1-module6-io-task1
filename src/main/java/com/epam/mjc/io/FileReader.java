cdca3eb1a0772b205ef41126f68637e4aa1b4edd
Andreyome <andreyomem@gmail.com>
2023-01-07 19:26:10 +0200

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -11,10 +11,10 @@ public class FileReader {
             String[] line1 = (reader.readLine()).split(": ");
             int age = Integer.parseInt(line1[1]);
             String[] line2 = (reader.readLine()).split(": ");
-            String e_mail = line2[1];
+            String email = line2[1];
             String[] line3 = (reader.readLine()).split(": ");
             long phone = Long.parseLong(line3[1]);
-            return new Profile(name, age, e_mail, phone);
+            return new Profile(name, age, email, phone);
         } catch (IOException e) {
             e.printStackTrace();
         }
