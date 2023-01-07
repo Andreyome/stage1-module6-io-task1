bf8510fe28b0fe3e3833102bcff7fc4b2db9cde3
Andreyome <andreyomem@gmail.com>
2023-01-07 19:19:03 +0200

    changed position of closing

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -8,19 +8,18 @@ public class FileReader {
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new java.io.FileReader("src/main/resources/Profile.txt"));
-            String [] line = (reader.readLine()).split(": ");
-            String name =line [1];
-            String [] line1 = (reader.readLine()).split(": ");
-            int age = Integer.parseInt(line1 [1]);
-            String [] line2 = (reader.readLine()).split(": ");
-            String  e_mail = line2 [1];
-            String [] line3 = (reader.readLine()).split(": ");
-            long phone = Long.parseLong(line3 [1]);
-            System.out.println(name +" "+ age+" "+e_mail+" "+phone);
-            return new Profile(name,age,e_mail,phone);
+            String[] line = (reader.readLine()).split(": ");
+            String name = line[1];
+            String[] line1 = (reader.readLine()).split(": ");
+            int age = Integer.parseInt(line1[1]);
+            String[] line2 = (reader.readLine()).split(": ");
+            String e_mail = line2[1];
+            String[] line3 = (reader.readLine()).split(": ");
+            long phone = Long.parseLong(line3[1]);
+            System.out.println(name + " " + age + " " + e_mail + " " + phone);
             reader.close();
-        }
-        catch (IOException e){
+            return new Profile(name, age, e_mail, phone);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return new Profile();
